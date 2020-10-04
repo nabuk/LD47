034aa5cf048b0e0505dceb72c478f0923f2cc4df
Jakub Niemyjski <niemyjski.jakub@gmail.com>
2020-10-04 21:46:08 +0200

    windows icon

--- Assets/Scripts/CursorController.cs
+++ Assets/Scripts/CursorController.cs
@@ -4,12 +4,32 @@ public class CursorController : MonoBehaviour
 {
     void Awake()
     {
+#if UNITY_WEBGL
+        Cursor.SetCursor(new Texture2D(32, 32), new Vector2(0, 0), CursorMode.ForceSoftware);
+#else
+        //Cursor.SetCursor(cursorTexture, hotSpot, CursorMode.Auto);
+#endif
+
+        Cursor.visible = false;
+
+
+
+    }
+
+    void Start()
+    {
+#if UNITY_WEBGL
+        Cursor.SetCursor(new Texture2D(32, 32), new Vector2(0, 0), CursorMode.ForceSoftware);
+#else
+        //Cursor.SetCursor(cursorTexture, hotSpot, CursorMode.Auto);
+#endif
+
         Cursor.visible = false;
     }
 
     void Update()
     {
-        if (Cursor.visible)
+        //if (Cursor.visible)
             Cursor.visible = false;
 
         var cursorPosInWorld = Camera.main.ScreenToWorldPoint(Input.mousePosition);
