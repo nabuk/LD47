ec90d813afe35fafe3c4ba32f51733e4a0438314
Jakub Niemyjski <niemyjski.jakub@gmail.com>
2020-10-04 11:55:55 +0200

    graphics

--- Assets/Scripts/PlaythroughMode.cs
+++ Assets/Scripts/PlaythroughMode.cs
@@ -4,7 +4,7 @@ public class PlaythroughMode : MonoBehaviour
 {
     public void BeginMode()
     {
-        timer.SetTime(1 * 5);
+        timer.SetTime(1 * 60);
         timer.StartTime();
         crewCapsule.BeginPlaythrough();
         asteroidSpawner.StartSpawningForPlaythroughMode(gameTimeSec);
