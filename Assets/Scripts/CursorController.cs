d01bdfaeaf4bd586ff9493eee3c1bfcc92d6507d
Jakub Niemyjski <niemyjski.jakub@gmail.com>
2020-10-04 12:58:13 +0200

    stars

--- Assets/Scripts/CursorController.cs
+++ Assets/Scripts/CursorController.cs
@@ -2,5 +2,17 @@
 
 public class CursorController : MonoBehaviour
 {
-    
+    void Awake()
+    {
+        Cursor.visible = false;
+    }
+
+    void Update()
+    {
+        if (Cursor.visible)
+            Cursor.visible = false;
+
+        var cursorPosInWorld = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        transform.position = (Vector2)cursorPosInWorld;
+    }
 }
---
+++ Assets/Scripts/Star.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class Star : MonoBehaviour
+{
+    SpriteRenderer spriteRenderer = default;
+
+    void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+
+        var zRotation = Random.Range(0f, 360f);
+        var xScale = Random.Range(0.5f, 1f);
+        var yScale = Random.Range(0.5f, 1f);
+        var brightness = Random.Range(0.2f, 0.7f);
+
+        transform.eulerAngles = new Vector3(0, 0, zRotation);
+        transform.localScale = new Vector3(xScale, yScale, 1);
+        spriteRenderer.color = new Color(brightness, brightness, brightness);
+    }
+}
